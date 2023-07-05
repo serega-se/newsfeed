422b454843f1c1fed5b8bf342f215681d5ee3153
se <se@sfhub.ru>
2023-07-05 18:40:43 +0700

    init

---
+++ news-feed/app/Filament/Resources/NewsResource.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\NewsResource\Pages;
+use App\Filament\Resources\NewsResource\RelationManagers;
+use App\Models\News;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class NewsResource extends Resource
+{
+    protected static ?string $model = News::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Card::make()->schema([
+                    Forms\Components\RichEditor::make('content')
+                        ->required()
+                        ->label('Содержание'),
+                    Forms\Components\FileUpload::make('img')
+                        ->image()
+                        ->imageResizeMode('cover')
+                        ->imageCropAspectRatio('16:9')
+                        ->imageResizeTargetWidth('1920')
+                        ->imageResizeTargetHeight('1080')
+                        ->label('Прикрепить файл'),
+                    Forms\Components\DateTimePicker::make('publishTo')
+                        ->label('Опубликовать с'),
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\ImageColumn::make('img')
+                    ->label('Файл'),
+                Tables\Columns\TextColumn::make('content')
+                    ->sortable()
+                    ->searchable()
+                    ->label('Содержание'),
+                Tables\Columns\TextColumn::make('publishTo')
+                    ->sortable()
+                    ->searchable()
+                    ->label('Опубликовать с'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListNews::route('/'),
+            'create' => Pages\CreateNews::route('/create'),
+            'edit' => Pages\EditNews::route('/{record}/edit'),
+        ];
+    }
+}
